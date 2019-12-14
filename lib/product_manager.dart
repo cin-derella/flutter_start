54d85d55a3b54941dddfd67bb9c853cff55541e6
cin-derella <shirley_cinderella@outlook.com>
2019-12-14 18:04:59 +0800

    pass data to stateful widget

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
+  final String startingProduct;
+  ProductManager(this.startingProduct);
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -10,7 +12,12 @@ class ProductManager extends StatefulWidget {
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Dark Forest'];
+  List<String> _products = [];
+  @override
+  void initState() {
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
