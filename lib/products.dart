8211a4530b46f4142497bf17bd9d60a6167aa23b
cin-derella <shirley_cinderella@outlook.com>
2019-12-15 12:27:51 +0800

    different ways to render conditional content

--- lib/products.dart
+++ lib/products.dart
@@ -1,29 +1,40 @@
 import 'package:flutter/material.dart';
 
 class Products extends StatelessWidget {
-  final List <String> products;
-  Products([this.products=const []]){
+  final List<String> products;
+  Products([this.products = const []]) {
     print('[Products Widget] Constructor');
-
   }
 
-Widget _buildProductItem(BuildContext context,int index){
-  return Card(
-    child: Column
-      (children: <Widget>[
-        Image.asset('assets/darkforest.jpg'),
-        Text(products[index])
-      ],
-    ),
-  );
-}
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/darkforest.jpg'),
+          Text(products[index])
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductLists(){
+      
+    Widget productCard;
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    } else {
+      productCard = Container();
+      //Center(child: Text('No books found, please add some'),);
+    }
+    return productCard;
+  }
+
   @override
   Widget build(BuildContext context) {
     print('[Products Widget] build()');
-
-    return products.length >0  ? ListView.builder(
-      itemBuilder: _buildProductItem,
-      itemCount: products.length,
-    ) : Center(child:Text('No books found, please add some'));
+    return _buildProductLists();
   }
 }
