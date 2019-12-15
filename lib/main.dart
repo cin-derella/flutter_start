965ef386ce02626d734ab0fe520f0b7f8cf3591e
cin-derella <shirley_cinderella@outlook.com>
2019-12-15 14:06:03 +0800

    add multiple pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,20 @@
 import 'package:flutter/material.dart';
 import './product_manager.dart';
-//import './products.dart';
+import './pages/home.dart';
 
 main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme:ThemeData(
-
-       brightness: Brightness.light,
-       primarySwatch: Colors.deepOrange,
-       accentColor: Colors.deepPurple
-      ),
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('EasyList'),
-      ),
-      body: ProductManager(),
-      ),
-      
-      );
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple),
+      home: HomePage(),
+    );
   }
 }
