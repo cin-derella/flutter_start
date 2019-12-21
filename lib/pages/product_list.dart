25e326a22dac30ec79663aba0a22e5c86f6d72c0
cin-derella <shirley_cinderella@outlook.com>
2019-12-21 12:14:53 +0800

    add dismissible function

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -4,29 +4,54 @@ import './product_edit.dart';
 
 class ProductListPage extends StatelessWidget {
   final Function updateProduct;
+  final Function deleteProdut;
   final List<Map<String, dynamic>> products;
-  ProductListPage(this.products,this.updateProduct);
+  ProductListPage(this.products, this.updateProduct,this.deleteProdut);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          leading: Image.asset(products[index]['image']),
-          title: Text(products[index]['title']),
-          trailing: IconButton(
-            icon: Icon(Icons.edit),
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (BuildContext context) {
-                    return ProductEditPage(product:products[index],updateProduct: updateProduct,productIndex: index,);
+        return Dismissible(
+            key: Key(products[index]['title']),
+            onDismissed: (DismissDirection direction){
+              if(direction == DismissDirection.endToStart){
+                deleteProdut(index);
+                print('Swiped end to start');
+              }else if(direction ==DismissDirection.startToEnd){
+                print('Swiped start to end');
+              }else{
+                print('Other swiping');
+              }
+            },
+            background: Container(color: Colors.red),
+
+            child: Column(children: <Widget>[
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(products[index]['image']),
+                ),
+                title: Text(products[index]['title']),
+                subtitle: Text('\¥${products[index]['price'].toString()}'),
+                trailing: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (BuildContext context) {
+                          return ProductEditPage(
+                            product: products[index],
+                            updateProduct: updateProduct,
+                            productIndex: index,
+                          );
+                        },
+                      ),
+                    );
                   },
                 ),
-              );
-            },
-          ),
-        );
+              ),
+              Divider()
+            ]));
       },
       itemCount: products.length,
     );
