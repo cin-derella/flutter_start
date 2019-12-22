636dbb273808ad5b98b559f32cedf5c82ad5b487
cin-derella <shirley_cinderella@outlook.com>
2019-12-22 11:39:38 +0800

    create toggle favorite method

--- lib/scoped-models/products.dart
+++ lib/scoped-models/products.dart
@@ -2,40 +2,54 @@ import 'package:scoped_model/scoped_model.dart';
 import '../models/product.dart';
 
 class ProductsModel extends Model {
-   List<Product> _products = [];
-   int _selectedProductIndex;
+  List<Product> _products = [];
+  int _selectedProductIndex;
 
-   List<Product> get products {
-     return List.from(_products);
-   }
+  List<Product> get products {
+    return List.from(_products);
+  }
 
-  int get selectedProductIndex{
+  int get selectedProductIndex {
     return _selectedProductIndex;
   }
 
-  Product get selectedProduct{
-    if(_selectedProductIndex == null){
+  Product get selectedProduct {
+    if (_selectedProductIndex == null) {
       return null;
     }
     return _products[_selectedProductIndex];
   }
 
   void addProduct(Product product) {
-      _products.add(product);
-      _selectedProductIndex = null;
+    _products.add(product);
+    _selectedProductIndex = null;
   }
 
-  void updateProduct(Product product){
-      _products[_selectedProductIndex]= product;
-      _selectedProductIndex = null;
+  void updateProduct(Product product) {
+    _products[_selectedProductIndex] = product;
+    _selectedProductIndex = null;
   }
 
   void deleteProduct(int index) {
-      _products.removeAt(_selectedProductIndex);
-      _selectedProductIndex = null;
-}
+    _products.removeAt(_selectedProductIndex);
+    _selectedProductIndex = null;
+  }
 
-  void selectProduct(int index){
-    _selectedProductIndex =index;
+  void toggleProductFavoriteStatus() {
+    final bool isCurrentlyFavorite =
+        _products[_selectedProductIndex].isFavorite;
+    final bool newFavoriteStatus = !isCurrentlyFavorite;
+    final Product updatedProduct = Product(
+        title: selectedProduct.title,
+        description: selectedProduct.description,
+        price: selectedProduct.price,
+        image: selectedProduct.image,
+        isFavorite: newFavoriteStatus);
+    _products[_selectedProductIndex] = updatedProduct;
+    _selectedProductIndex = null;
+  }
+
+  void selectProduct(int index) {
+    _selectedProductIndex = index;
   }
 }
