2ee2639241a42b774c2e1784211ba716c97b002e
cin-derella <shirley_cinderella@outlook.com>
2019-12-22 14:43:35 +0800

    add notify listener

--- lib/widgets/products/product_card.dart
+++ lib/widgets/products/product_card.dart
@@ -3,6 +3,8 @@ import './price_tag.dart';
 import '../ui_elements/title_default.dart';
 import './address_tag.dart';
 import '../../models/product.dart';
+import 'package:scoped_model/scoped_model.dart';
+import '../../scoped-models/products.dart';
 
 class ProductCard extends StatelessWidget {
   final Product product;
@@ -35,11 +37,18 @@ class ProductCard extends StatelessWidget {
             color: Theme.of(context).accentColor,
             onPressed: () => Navigator.pushNamed<bool>(
                 context, '/product/' + productIndex.toString())),
-        IconButton(
-            icon: Icon(Icons.favorite_border),
-            color: Colors.red,
-            onPressed: () => Navigator.pushNamed<bool>(
-                context, '/product/' + productIndex.toString()))
+        ScopedModelDescendant<ProductsModel>(
+            builder: (BuildContext context, Widget child, ProductsModel model) {
+          return IconButton(
+              icon: Icon(model.products[productIndex].isFavorite
+                  ? Icons.favorite
+                  : Icons.favorite_border),
+              color: Colors.red,
+              onPressed: () {
+                model.selectProduct(productIndex);
+                model.toggleProductFavoriteStatus();
+              });
+        })
       ],
     );
   }
