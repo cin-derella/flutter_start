f03f9dabf49913bc9a3618c115b64bd693a9b8a1
cin-derella <shirley_cinderella@outlook.com>
2019-12-29 11:16:23 +0800

    add a sign up function

--- lib/scoped-models/connected_products.dart
+++ lib/scoped-models/connected_products.dart
@@ -230,8 +230,35 @@ class ProductsModel extends ConnectedProductsModel {
 }
 
 class UserModel extends ConnectedProductsModel {
-  void login(String email, String password) {
-    _authenticatedUser = User(id: '123', email: email, password: password);
+  Future<Map<String,dynamic>> login(String email, String password) async{
+    _isLoading = true;
+    notifyListeners();
+    final Map<String, dynamic> authData = {
+      'email': email,
+      'password': password,
+      'returnSecureToken': true
+    };
+
+    final http.Response response = await http.post(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDDPaNyzR79YzBH7Q6xvPphmxLCgMbcEBI',
+      body: json.encode(authData),
+      headers:{'Content-Type':'application/json'});
+          final Map<String, dynamic> responseData = json.decode(response.body);
+    bool hasError = true;
+    String message = 'Something went wrong.';
+    print(responseData);
+    if (responseData.containsKey('idToken')) {
+      hasError = false;
+      message = 'Authentication succeeded!';
+    } else if (responseData['error']['message'] == 'EMAIL_NOT_FOUND') {
+      message = 'This email was not found.';
+    }else if (responseData['error']['message'] == 'INVALID_PASSWORD') {
+      message = 'This password is invalid.';
+    }
+    _isLoading = false;
+    notifyListeners();
+    return {'success': !hasError, 'message': message};
+  
   }
 
   Future<Map<String, dynamic>> signup(String email, String password) async {
@@ -257,7 +284,7 @@ class UserModel extends ConnectedProductsModel {
     } else if (responseData['error']['message'] == 'EMAIL_EXISTS') {
       message = 'This email already exists.';
     }
-    _isLoading=false;
+    _isLoading = false;
     notifyListeners();
     return {'success': !hasError, 'message': message};
   }
