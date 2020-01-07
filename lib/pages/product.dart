7686b360d9657f569bc6e94751410f6f72d64e78
cin-derella <shirley_cinderella@outlook.com>
2020-01-07 22:41:17 +0800

    add the sliver effect

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -89,42 +89,54 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-        onWillPop: () {
-          print('Back button pressed');
-          Navigator.pop(context, false);
-          return Future.value(false);
-        },
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text(product.title),
-          ),
-          body: Column(
-            //mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Hero(
-                tag:product.id,
-              child:FadeInImage(
-                image: NetworkImage(product.image),
-                height: 300.0,
-                fit: BoxFit.cover,
-                placeholder: AssetImage('assets/background.jpg'),
-              ),),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                child: TitleDefault(product.title),
+      onWillPop: () {
+        print('Back button pressed');
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        // appBar: AppBar(
+        //   title: Text(product.title),
+        // ),
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              expandedHeight: 256.0,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text(product.title),
+                background: Hero(
+                  tag: product.id,
+                  child: FadeInImage(
+                    image: NetworkImage(product.image),
+                    height: 300.0,  
+                    fit: BoxFit.cover,
+                    placeholder: AssetImage('assets/background.jpg'),
+                  ),
+                ),
               ),
-              _buildAddressPriceRow(product.location.address, product.price),
-              Container(
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate([
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  alignment: Alignment.center,
+                  child: TitleDefault(product.title),
+                ),
+                _buildAddressPriceRow(product.location.address, product.price),
+                Container(
                   padding: EdgeInsets.all(10.0),
                   child: Text(
                     product.description,
                     textAlign: TextAlign.center,
-                  ))
-            ],
-          ),
-          floatingActionButton: ProductFAB(product),
-          //Center(child: Text('On the Book Page'),),
-        ));
+                  ),
+                )
+              ]),
+            ),
+          ],
+        ),
+        floatingActionButton: ProductFAB(product),
+      ), //Center(child: Text('On the Book Page'),),
+    );
   }
 }
